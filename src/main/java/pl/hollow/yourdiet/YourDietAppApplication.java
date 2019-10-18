e3a30df3332d3800578f659b0dfef53baddd61f1
ffabisiakk <ffabisiakk@gmail.com>
2019-10-18 17:47:36 +0200

    init

---
+++ src/main/java/pl/hollow/yourdiet/YourDietAppApplication.java
@@ -0,0 +1,13 @@
+package pl.hollow.yourdiet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourDietAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourDietAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/hollow/yourdiet/YourDietAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.hollow.yourdiet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourDietAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
